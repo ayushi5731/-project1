7d58c947d0a3779eaaed407710308532e71b40ed
Takshak Mudgal <takshakmudgal@gmail.com>
2024-12-09 22:51:03 +0530

    fix: responsiveness

--- frontend/src/components/Section1.js
+++ frontend/src/components/Section1.js
@@ -4,29 +4,35 @@ import '../styles/Section1.css'
 function Section1() {
   return (
     <div className="outer">
-  <div className="inner">
-    <h5>Unified Dashboard</h5>
-    <p>Acentralized hub where radiologists,
-       admins, and patients can view
-       pertinentinformation,track case
-       statuses, and monitor workflow</p>
-  </div>
-  <div className="inner">
-    <h5>Unified Dashboard</h5>
-    <p>Acentralized hub where radiologists,
-       admins, and patients can view
-       pertinentinformation,track case
-       statuses, and monitor workflow</p>
-  </div>
-  <div className="inner">
-    <h5>Unified Dashboard</h5>
-    <p>Acentralized hub where radiologists,
-       admins, and patients can view
-       pertinentinformation,track case
-       statuses, and monitor workflow</p>
-  </div>
-</div>
-
-  )
+      <div className="inner">
+        <h5>Unified Dashboard</h5>
+        <p>
+          A centralized hub where radiologists,
+          admins, and patients can view
+          pertinent information, track case
+          statuses, and monitor workflow
+        </p>
+      </div>
+      <div className="inner">
+        <h5>Unified Dashboard</h5>
+        <p>
+          A centralized hub where radiologists,
+          admins, and patients can view
+          pertinent information, track case
+          statuses, and monitor workflow
+        </p>
+      </div>
+      <div className="inner">
+        <h5>Unified Dashboard</h5>
+        <p>
+          A centralized hub where radiologists,
+          admins, and patients can view
+          pertinent information, track case
+          statuses, and monitor workflow
+        </p>
+      </div>
+    </div>
+  );
 }
+
 export default Section1;
